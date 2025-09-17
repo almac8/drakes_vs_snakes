a2938f3bd326cd7b80547bac0d2426c4329e6b62
Aimée MacDonald <aimeelmacdonald@pm.me>
2025-09-17 18:13:07 +0200

    Terminal print conceptual map

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-  println!("Drakes VS Snakes");
+  println!("");
+  println!("");
+  println!("_ _ _ _ _ _ _ _");
+  println!("_ P _ _ 1 1 1 _");
+  println!("_ * 1 1 2 S 1 _");
+  println!("_ * 1 S 2 1 1 _");
+  println!("_ * 2 2 2 _ _ _");
+  println!("_ * 2 S 1 _ _ _");
+  println!("_ * S * * G _ _");
+  println!("_ * * * _ _ _ _");
+  println!("");
+  println!("");
 }
