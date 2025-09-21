710eae14556466aa2258c66bc946b8d1d4ff1f4d
Aimée MacDonald <aimeelmacdonald@pm.me>
2025-09-21 20:31:34 +0200

    Print player and goal

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,17 @@ fn main() {
   let map_width = 8;
   let map_height = 8;
 
+  let player_index = 17;
+  let goal_index = 54;
+
   for index in 0..(map_width * map_height) {
-    print!("_");
+    if index == player_index {
+      print!("P");
+    } else if index == goal_index {
+      print!("G");
+    } else {
+      print!("_");
+    }
 
     if index % map_width == map_width - 1 {
       println!();
