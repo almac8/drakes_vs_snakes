ea71738d1cc12f0a041998113ade6eb374517623
Aimée MacDonald <aimeelmacdonald@pm.me>
2025-09-21 20:42:50 +0200

    Game Loop

--- src/main.rs
+++ src/main.rs
@@ -5,40 +5,47 @@ fn main() {
   let player_index = 17;
   let goal_index = 54;
 
-  for index in 0..(map_width * map_height) {
-    if index == player_index {
-      print!("P");
-    } else if index == goal_index {
-      print!("G");
-    } else {
-      print!("_");
-    }
+  let mut is_running = true;
 
-    if index % map_width == map_width - 1 {
-      println!();
-    } else {
-      print!(" ");
-    }
-  }
-
-  let mut input_buffer = String::new();
-  std::io::stdin()
-    .read_line(&mut input_buffer)
-    .expect("Input Error");
+  while is_running {
+    for index in 0..(map_width * map_height) {
+      if index == player_index {
+        print!("P");
+      } else if index == goal_index {
+        print!("G");
+      } else {
+        print!("_");
+      }
   
-  let input = match input_buffer.trim().parse() {
-    Ok(num) => num,
-    Err(_) => {
-      println!("Invalid input. Please enter a number.");
-      0
+      if index % map_width == map_width - 1 {
+        println!();
+      } else {
+        print!(" ");
+      }
     }
-  };
+  
+    let mut input_buffer = String::new();
+    std::io::stdin()
+      .read_line(&mut input_buffer)
+      .expect("Input Error");
+    
+    let input = match input_buffer.trim().parse() {
+      Ok(num) => num,
+      Err(_) => {
+        println!("Invalid input. Please enter a number.");
+        0
+      }
+    };
+  
+    match input {
+      5555 => is_running = false,
 
-  match input {
-    8 => println!("North"),
-    4 => println!("West"),
-    6 => println!("East"),
-    2 => println!("South"),
-    _ => println!("Invalid input"),
+      8 => println!("North"),
+      4 => println!("West"),
+      6 => println!("East"),
+      2 => println!("South"),
+      
+      _ => println!("Invalid input"),
+    }
   }
 }
