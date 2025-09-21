0a6bb4d0f3439e2a81f6f89e55604b6329d95632
Aimée MacDonald <aimeelmacdonald@pm.me>
2025-09-21 20:28:15 +0200

    Print map with correct width and height

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-  println!("Drakes VS Snakes");
+  let map_width = 8;
+  let map_height = 8;
+
+  for index in 0..(map_width * map_height) {
+    print!("_");
+
+    if index % map_width == map_width - 1 {
+      println!();
+    } else {
+      print!(" ");
+    }
+  }
 }
