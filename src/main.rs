cd240448d7bb0653f394d050c3dcd3841b6b3669
Aimée MacDonald <aimeelmacdonald@pm.me>
2025-09-21 21:47:18 +0200

    Snake hints

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,44 @@ fn main() {
   let goal_index = calculate_index_from_coordinates(goal_location_x, goal_location_y, map_width);
 
   let mut is_snake = Vec::new();
+  let mut snake_hints = Vec::new();
 
   for _ in 0..(map_width * map_height) {
     is_snake.push(false);
+    snake_hints.push(0);
   }
 
-  is_snake[calculate_index_from_coordinates(1, 1, map_width)] = true;
-  is_snake[calculate_index_from_coordinates(3, 3, map_width)] = true;
-  is_snake[calculate_index_from_coordinates(5, 5, map_width)] = true;
-  is_snake[calculate_index_from_coordinates(7, 7, map_width)] = true;
+  let snake_location_x = 1;
+  let snake_location_y = 1;
+  is_snake[calculate_index_from_coordinates(snake_location_x, snake_location_y, map_width)] = true;
+  let snake_neighbors = get_direct_neighbors(snake_location_x, snake_location_y, map_width, map_height);
+  for neighbor_index in snake_neighbors {
+    snake_hints[neighbor_index] += 1;
+  }
+
+  let snake_location_x = 3;
+  let snake_location_y = 3;
+  is_snake[calculate_index_from_coordinates(snake_location_x, snake_location_y, map_width)] = true;
+  let snake_neighbors = get_direct_neighbors(snake_location_x, snake_location_y, map_width, map_height);
+  for neighbor_index in snake_neighbors {
+    snake_hints[neighbor_index] += 1;
+  }
+
+  let snake_location_x = 5;
+  let snake_location_y = 5;
+  is_snake[calculate_index_from_coordinates(snake_location_x, snake_location_y, map_width)] = true;
+  let snake_neighbors = get_direct_neighbors(snake_location_x, snake_location_y, map_width, map_height);
+  for neighbor_index in snake_neighbors {
+    snake_hints[neighbor_index] += 1;
+  }
+
+  let snake_location_x = 6;
+  let snake_location_y = 6;
+  is_snake[calculate_index_from_coordinates(snake_location_x, snake_location_y, map_width)] = true;
+  let snake_neighbors = get_direct_neighbors(snake_location_x, snake_location_y, map_width, map_height);
+  for neighbor_index in snake_neighbors {
+    snake_hints[neighbor_index] += 1;
+  }
 
   let mut is_running = true;
   while is_running {
@@ -31,7 +60,7 @@ fn main() {
       } else if is_snake[index] {
         print!("S");
       } else {
-        print!("_");
+        print!("{}", snake_hints[index]);
       }
   
       if index % map_width == map_width - 1 {
@@ -92,4 +121,26 @@ fn main() {
 
 fn calculate_index_from_coordinates(x: usize, y: usize, map_width: usize) -> usize {
   y * map_width + x
+}
+
+fn get_direct_neighbors(location_x: usize, location_y: usize, map_width: usize, map_height: usize) -> Vec<usize> {
+  let mut neighbors = Vec::new();
+
+  if location_y > 0 {
+    neighbors.push(calculate_index_from_coordinates(location_x, location_y - 1, map_width));
+  }
+
+  if location_x > 0 {
+    neighbors.push(calculate_index_from_coordinates(location_x - 1, location_y, map_width));
+  }
+
+  if location_x < map_width - 1 {
+    neighbors.push(calculate_index_from_coordinates(location_x + 1, location_y, map_width));
+  }
+
+  if location_y < map_height - 1 {
+    neighbors.push(calculate_index_from_coordinates(location_x, location_y + 1, map_width));
+  }
+  
+  neighbors
 }
