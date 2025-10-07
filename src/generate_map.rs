7d72d6e1f8284da2ada9413495200484bade192b
Aimée MacDonald <aimeelmacdonald@pm.me>
2025-10-07 11:29:16 +0200

    Bad seed regeneration bug

--- src/generate_map.rs
+++ src/generate_map.rs
@@ -17,35 +17,41 @@ pub fn generate_map(size: MapSize, num_snakes: usize, rng: &mut rand::rngs::StdR
   let mut map = Map::new();
   map.size = size;
 
-  map.player_location = Coordinate::from(
-    rng.random_range(0..(map.size.width() / 3)),
-    rng.random_range(0..(map.size.height() / 3)),
-    &map.size
-  );
-
-  map.goal_location = Coordinate::from(
-    rng.random_range((map.size.width() / 3 * 2)..(map.size.width() - 1)),
-    rng.random_range((map.size.height() / 3 * 2)..(map.size.height() - 1)),
-    &map.size
-  );
+  let mut map_is_valid = false;
+  while !map_is_valid {
+    map.player_location = Coordinate::from(
+      rng.random_range(0..(map.size.width() / 3)),
+      rng.random_range(0..(map.size.height() / 3)),
+      &map.size
+    );
   
-  map.is_snake = generate_snakes(&map, num_snakes, rng);
-  map.hint = generate_hints(&map);
-
-  *map.score.mut_maximum() = calculate_max_score(&map);
-  match find_path(&map) {
-    Ok(path) => map.is_path = path,
-
-    Err(error) => if error == "KJ".to_string() {
-      generate_map(size, num_snakes, rng)?;
+    map.goal_location = Coordinate::from(
+      rng.random_range((map.size.width() / 3 * 2)..(map.size.width() - 1)),
+      rng.random_range((map.size.height() / 3 * 2)..(map.size.height() - 1)),
+      &map.size
+    );
+    
+    map.is_snake = generate_snakes(&map, num_snakes, rng);
+    map.hint = generate_hints(&map);
+  
+    *map.score.mut_maximum() = calculate_max_score(&map);
+    match find_path(&map) {
+      Ok(path) => {
+        map.is_path = path;
+        map_is_valid = true;
+      },
+  
+      Err(error) => if error == "Unable to find a next step".to_string() {
+        map_is_valid = false;
+      }
     }
+  
+    map.is_marked = vec![false; map.size.array_length()];
+  
+    map.is_explored = vec![false; map.size.array_length()];
+    let player_index_buffer = map.player_location.array_index();
+    map.is_explored[player_index_buffer] = true;
   }
-
-  map.is_marked = vec![false; map.size.array_length()];
-
-  map.is_explored = vec![false; map.size.array_length()];
-  let player_index_buffer = map.player_location.array_index();
-  map.is_explored[player_index_buffer] = true;
 
   Ok(map)
 }
@@ -147,16 +153,55 @@ use crate::MapSize;
     match generate_map(size, num_snakes, &mut rng) {
       Ok(map) => {
         assert_eq!(map.size, size);
-        assert_ne!(map.player_location.array_index(), 1);
-        assert_ne!(map.goal_location.array_index(), 0);
+        assert_eq!(map.player_location.array_index(), 1);
+        assert_eq!(map.goal_location.array_index(), 36);
         assert_eq!(map.score.current(), 0);
-        assert_ne!(map.score.maximum(), 0);
-
-        assert_ne!(map.hint, vec![0; 64]);
-        assert_ne!(map.is_snake, vec![false; 64]);
+        assert_eq!(map.score.maximum(), 73);
         assert_eq!(map.is_marked, vec![false; 64]);
-        assert_ne!(map.is_explored, vec![false; 64]);
-        assert_ne!(map.is_path, vec![false; 64]);
+        
+        assert_eq!(map.is_explored, vec![
+          false,  true, false, false, false, false, false, false,
+          false, false, false, false, false, false, false, false,
+          false, false, false, false, false, false, false, false,
+          false, false, false, false, false, false, false, false,
+          false, false, false, false, false, false, false, false,
+          false, false, false, false, false, false, false, false,
+          false, false, false, false, false, false, false, false,
+          false, false, false, false, false, false, false, false
+        ]);
+        
+        assert_eq!(map.is_snake, vec![
+          false, false, false,  true,  true, false, false, false,
+          false, false, false,  true,  true,  true,  true, false,
+          false, false, false,  true,  true,  true, false, false,
+          false,  true, false, false, false, false, false, false,
+           true, false, false, false, false, false, false, false,
+          false, false,  true, false, false, false,  true, false,
+          false, false,  true, false,  true,  true, false, false,
+          false, false, false, false, false, false, false, false
+        ]);
+        
+        assert_eq!(map.hint, vec![
+          0, 0, 2, 3, 4, 4, 2, 1,
+          0, 0, 3, 5, 7, 5, 2, 1,
+          1, 1, 3, 3, 5, 4, 3, 1,
+          2, 1, 2, 2, 3, 2, 1, 0,
+          1, 3, 2, 1, 0, 1, 1, 1,
+          1, 3, 1, 3, 2, 3, 1, 1,
+          0, 2, 1, 3, 1, 2, 2, 1,
+          0, 1, 1, 2, 2, 2, 1, 0
+        ]);
+        
+        assert_eq!(map.is_path, vec![
+          false,  true, false, false, false, false, false, false,
+          false,  true, false, false, false, false, false, false,
+          false,  true,  true, false, false, false, false, false,
+          false, false,  true, false, false, false, false, false,
+          false, false,  true,  true,  true, false, false, false,
+          false, false, false, false, false, false, false, false,
+          false, false, false, false, false, false, false, false,
+          false, false, false, false, false, false, false, false
+        ]);
       },
 
       Err(error) => panic!("Unexpected error: {}", error)
