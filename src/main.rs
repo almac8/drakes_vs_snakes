2067437c86451e024cb2271a02f1689233fa370d
Aimée MacDonald <aimeelmacdonald@pm.me>
2025-11-08 11:50:16 +0200

    Render shadow

--- src/main.rs
+++ src/main.rs
@@ -258,6 +258,9 @@ use resolution::Resolution;
 mod vector2;
 use vector2::Vector2;
 
+mod render_shadow;
+use render_shadow::render_shadow;
+
 fn main() -> Result<(), String> {
   let sdl_context = sdl2::init()?;
   let video_subsystem = sdl_context.video()?;
@@ -815,89 +818,4 @@ impl Camera {
   fn projection_matrix(&self) -> &Matrix4 {
     &self.projection_matrix
   }
-}
-
-fn render_shadow(shadow_bits: &[bool; 4], shadow_sprites: &mut ShadowSprites, camera: &Camera, quad_shader_program: &ShaderProgram) -> Result<(), String> {
-  match shadow_bits {
-    [false, false, false, false] => {
-      render_sprite(shadow_sprites.zero(), camera, quad_shader_program)?;
-    },
-    
-    [false, false, false, true] => {
-      shadow_sprites.mut_one().mut_transform().rotate_to(180.0);
-      render_sprite(&shadow_sprites.one(), &camera, &quad_shader_program)?;
-    },
-    
-    [false, false, true, false] => {
-      shadow_sprites.mut_one().mut_transform().rotate_to(90.0);
-      render_sprite(&shadow_sprites.one(), &camera, &quad_shader_program)?;
-    },
-    
-    [false, false, true, true] => {
-      shadow_sprites.mut_three().mut_transform().rotate_to(90.0);
-      render_sprite(&shadow_sprites.three(), &camera, &quad_shader_program)?;
-    },
-    
-    [false, true, false, false] => {
-      shadow_sprites.mut_one().mut_transform().rotate_to(270.0);
-      render_sprite(&shadow_sprites.one(), &camera, &quad_shader_program)?;
-    },
-    
-    [false, true, false, true] => {
-      shadow_sprites.mut_three().mut_transform().rotate_to(180.0);
-      render_sprite(&shadow_sprites.three(), &camera, &quad_shader_program)?;
-    },
-    
-    [false, true, true, false] => {
-      shadow_sprites.mut_two().mut_transform().rotate_to(90.0);
-      render_sprite(&shadow_sprites.two(), &camera, &quad_shader_program)?;
-    },
-    
-    [false, true, true, true] => {
-      shadow_sprites.mut_four().mut_transform().rotate_to(180.0);
-      render_sprite(&shadow_sprites.four(), &camera, &quad_shader_program)?;
-    },
-    
-    [true, false, false, false] => {
-      shadow_sprites.mut_one().mut_transform().rotate_to(0.0);
-      render_sprite(&shadow_sprites.one(), &camera, &quad_shader_program)?;
-    },
-    
-    [true, false, false, true] => {
-      shadow_sprites.mut_two().mut_transform().rotate_to(0.0);
-      render_sprite(&shadow_sprites.two(), &camera, &quad_shader_program)?;
-    },
-    
-    [true, false, true, false] => {
-      shadow_sprites.mut_three().mut_transform().rotate_to(0.0);
-      render_sprite(&shadow_sprites.three(), &camera, &quad_shader_program)?;
-    },
-    
-    [true, false, true, true] => {
-      shadow_sprites.mut_four().mut_transform().rotate_to(90.0);
-      render_sprite(&shadow_sprites.four(), &camera, &quad_shader_program)?;
-    },
-    
-    [true, true, false, false] => {
-      shadow_sprites.mut_three().mut_transform().rotate_to(270.0);
-      render_sprite(&shadow_sprites.three(), &camera, &quad_shader_program)?;
-    },
-    
-    [true, true, false, true] => {
-      shadow_sprites.mut_four().mut_transform().rotate_to(270.0);
-      render_sprite(&shadow_sprites.four(), &camera, &quad_shader_program)?;
-    },
-    
-    [true, true, true, false] => {
-      shadow_sprites.mut_four().mut_transform().rotate_to(0.0);
-      render_sprite(&shadow_sprites.four(), &camera, &quad_shader_program)?;
-    },
-    
-    [true, true, true, true] => {
-      shadow_sprites.mut_five().mut_transform().rotate_to(0.0);
-      render_sprite(&shadow_sprites.five(), &camera, &quad_shader_program)?;
-    }
-  }
-  
-  Ok(())
 }
